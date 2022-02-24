e43d746878a9bbcc147f950b9242cb6b06a312cc
Ad450 <adjeimanue@gmail.com>
2022-02-24 22:31:29 +0000

    events added to UI

--- frontend/src/features/vote/components/voting.tsx
+++ frontend/src/features/vote/components/voting.tsx
@@ -1,25 +1,47 @@
-import { Flex, Text, Button, HStack } from "@chakra-ui/react";
+import { Flex, Text, Button, HStack, Heading } from "@chakra-ui/react";
 import { Contract } from "ethers";
 import { getFromChain, sendToChain } from "../../../core/web3/web3";
 import contract from "../../../contracts/contracts/general_election.sol/GeneralElection.json";
+import { useState } from "react";
+import { CounterEvent } from "../../../core/events/app_events";
 
 export const Voting = () => {
+  const [votes, setVotes] = useState("");
+  const counterEvent = new CounterEvent();
+
+  // listen on voted
+  counterEvent.on("voted", async () => {
+    await getVotes();
+
+    await getVotesOfParty();
+  });
+
   // using test abi from greeting smart contract
   const abi = contract.abi;
-  const address: string = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  // put in contract address here
+  const address: string = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
-  const vote = async () => {
+  const vote = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const party: string = event.currentTarget.innerHTML;
+
     await sendToChain({
       address: address,
       abi: abi,
       contractMethod: async (contract: Contract) => {
         // make sure party passed in is specified in the array of parties
+
         try {
-          const voted = await contract.voteForParty("NPP");
+          const voted = await contract.voteForParty(party);
+          await voted.wait();
           if (voted) {
+            //counterEvent.emit("voted");
             alert("Thank you for voting");
           }
         } catch (error) {
+          console.log(error);
+
           alert("chain reverted");
         }
       },
@@ -33,7 +55,7 @@ export const Voting = () => {
       abi: abi,
       contractMethod: async (contract: Contract) => {
         const votes = await contract.getTotalVotes();
-        alert(votes);
+        setVotes(votes);
       },
     });
   };
@@ -43,8 +65,8 @@ export const Voting = () => {
       address: address,
       abi: abi,
       contractMethod: async (contract: Contract) => {
-        const votes = await contract.getPartyVotes("NPP");
-        alert(votes);
+        const votes = await contract.getPartyVotes("Party A");
+        //alert(votes);
       },
     });
   };
@@ -54,7 +76,8 @@ export const Voting = () => {
       <Flex direction="column" bg="gray.900" p={12} rounded={6}>
         <Text mb={4}> Vote for a party by clicking one of these </Text>
         <HStack>
-          <Button onClick={(e) => vote()} size="sm">
+          <Heading>{votes}</Heading>
+          <Button onClick={(e) => vote(e)} size="sm">
             Party A
           </Button>
           <Button onClick={(e) => getVotes()} size="sm">
